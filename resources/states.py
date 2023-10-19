20c7f72996dd173320c93d0f14ea6041ad6eb3f5
Alexashin <aleksashin.ar@gmail.com>
2023-10-19 22:00:09 +0300

    Добавить состояния для FSM

--- resources/states.py
+++ resources/states.py
@@ -1,1 +1,24 @@
 # Состояния для FSM и клавиатур
+from aiogram.fsm.state import State, StatesGroup
+
+
+# Выбор кастомной категории затрат
+class CustomCategories(StatesGroup):
+    wait_name = State()
+    choose_to_change = State()
+
+
+# Добавление/выбор скидочной карты
+class DiscountCard(StatesGroup):
+    wait_photo = State()
+    wait_name = State()
+
+
+# Предоставление доступка к таблице
+class TableAccess(StatesGroup):
+    wait_email = State()
+
+
+# Выбор категории при вручном внесении траты
+class ChooseCategory(StatesGroup):
+    wait_category = State()
