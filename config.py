e5bda74870d28f996ebca074372d603ad8f3e5b9
Alexashin <aleksashin.ar@gmail.com>
2023-10-21 12:35:46 +0300

    Добавить id таблицы-шаблона в конфиг

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 # Настройки и параметры
 
-#telegram_id админов
+# telegram_id админов
 ADMINS = []
 
-#Стандартное азвание для таблиц
+# Стандартное азвание для таблиц
 SHEETS_TITLE = 'EasyBudgetBot$'
+
+# id таблицы с шаблоном
+TEMPLATE_ID = '16q9w3CsGCOPp02g-YFtoepvxLuQ5lm4SZ91htJrg3Jc'
